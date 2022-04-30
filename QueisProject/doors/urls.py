02cdc6b9210a1fbe8186bed8737a6886c554f7ea
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-04-29 20:48:19 -0500

    Init Details Queis by Was Published Recently

--- QueisProject/doors/urls.py
+++ QueisProject/doors/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "queis"
 urlpatterns = [
     path("main", views.MainView.as_view(), name = "main"),
+    path("door/<int:pk>", views.DetailView.as_view(), name = "detail_door"),
 ]
--- QueisProject/doors/views.py
+++ QueisProject/doors/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.utils import timezone
 
-from .models import Quei
+from .models import Quei, QueiContent
 
 class MainView(generic.ListView):
     template_name = "doors/main.html"
@@ -12,3 +12,9 @@ class MainView(generic.ListView):
     def get_queryset(self):
         return Quei.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:10]
 
+class DetailView(generic.DetailView):
+    model = QueiContent
+    template_name = "doors/detail_door.html"
+
+    def get_queryset(self):
+        return Quei.objects.filter(pub_date__lte=timezone.now())
